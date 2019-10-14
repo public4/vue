42ba216f83fedbcdb147e2bc07d96e88dafdb1f0
Public4 <public4@ya.ru>
2019-10-14 23:03:55 +0500

    validation

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuelidate from 'vuelidate'
 import dateFilter from '@/filters/date.filter'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
@@ -9,6 +10,7 @@ import 'materialize-css/dist/js/materialize.min'
 Vue.config.productionTip = false
 
 Vue.filter('dateFilter', dateFilter)
+Vue.use(Vuelidate)
 
 new Vue({
   router,
